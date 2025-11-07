4fe4fd8a342dd2946e17782bad6573a53943f161
Kilo S <stern489@umn.edu>
2025-11-07 10:51:43 -0600

    Push JavaScript, CSS and HTML files to GitHub.

---
+++ script.js
@@ -0,0 +1,18 @@
+let view;
+
+require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
+  const webmap = new WebMap({
+    portalItem: {
+      id: "6b836b2859194fdfa156458f2d2842e9"
+    }
+  });
+
+  view = new MapView({
+    map: webmap,
+    container: "viewDiv"
+  });
+});
+
+document.getElementById("zoom_in").addEventListener("click", () => {view.zoom += 1;});
+
+document.getElementById("zoom_out").addEventListener("click", () => {view.zoom -= 1;});
